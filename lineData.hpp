f0a894d362c8a15862ea6543af8e818d4ac5b766
Damian Jasiński <dam03jas@gmail.com>
2025-01-19 17:01:02 +0100

    lineData

---
+++ lineData.hpp
@@ -0,0 +1,52 @@
+#ifndef LINEDATA_HPP
+#define LINEDATA_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+class LineData {
+public:
+
+    explicit LineData(const string& line);
+
+
+    LineData(ifstream& in);
+
+    void print() const;
+
+    void printData() const;
+
+    string printString();
+
+    void serialize(ofstream& out) const;
+
+    void deserialize(ifstream& in);
+
+    string getDate() const { return date; }
+
+    float getAutokonsumpcja() const { return autokonsumpcja; }
+
+    float getEksport() const { return eksport; }
+
+    float getImport() const { return import; }
+
+    float getPobor() const { return pobor; }
+
+    float getProdukcja() const { return produkcja; }
+
+private:
+    string date;
+    float autokonsumpcja;
+    float eksport;
+    float import;
+    float pobor;
+    float produkcja;
+};
+
+#endif 
