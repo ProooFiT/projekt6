4ec6d162187209df82951418fbb8e5888f5272a2
Damian Jasiński <dam03jas@gmail.com>
2025-01-19 17:01:44 +0100

    lineValidation

---
+++ lineValidation.hpp
@@ -0,0 +1,28 @@
+#ifndef LINEVALIDATION_HPP
+#define LINEVALIDATION_HPP
+
+#include <string>
+#include <cctype>
+#include <algorithm>
+
+#include "logger.hpp"
+
+bool lineValidation(const std::string& line) {
+    if (line.empty()) {
+        loggerError.log("Pusta linia");
+        return false;
+    } else if (line.find("Time") != std::string::npos) {
+        loggerError.log("Znaleziono nagłówek: " + line);
+        return false;
+    } else if (std::any_of(line.begin(), line.end(), [](char c) { return std::isalpha(c); })) {
+        loggerError.log("Znaleziono inne dane: " + line);
+        return false;
+    } else if (std::count(line.begin(), line.end(), ',') != 5) {
+        loggerError.log("Nieprawidłowa liczba parametrów: " + line);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+#endif 
